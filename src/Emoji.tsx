9864609108748e94d3ed7df140edd91cb1c76541
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-01-06 19:53:49 -0300

    cria Map de emojis

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,6 +1,29 @@
 import "./Emoji.css"
+const EMOJI = new Map<string, string>([
+    ["earth", "🌍"],
+    ["moon", "🌕"],
+    ["sun", "☀️"],
+])
 export default function Emoji() {
+    let situacao = ""
+    function toEarth() {
+        situacao = "earth"
+    }
+    function toMoon() {
+        situacao = "moon"
+    }
+    function toSun() {
+        situacao = "sun"
+    }
+    
     return (
-        <div className="emoji">🚀</div>
+        <div className="emoji">
+            <div className="planeta">{EMOJI.get(situacao)||"🚀"}</div>
+            <div className="acoes">
+                <button onClick={toEarth}>go to Earth</button>
+                <button onClick={toMoon}>go to Moon</button>
+                <button onClick={toSun}>go to Sun</button>
+            </div>
+        </div>
     )
 }
